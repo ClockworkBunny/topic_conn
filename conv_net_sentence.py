8b52119780b5e881099d255e341d6e85a5a2a75f
ClockBunny <rui91seu@gmail.com>
2015-05-21 14:48:15 +0800

    layer input construct

--- conv_net_sentence.py
+++ conv_net_sentence.py
@@ -117,7 +117,7 @@ def train_conv_net(datasets,datasets_weights,
     layer0_inputs_topics = []
     for i in range(num_topics):
         sin_topic = x_topic[:][:][i]
-        Topic = Topics.reshape((1,Topics[i].shape[0]))
+        Topic = Topics[i].reshape((1,Topics[i].shape[0]))
         weights = sin_topic.flatten()
         weights = weights.reshape((weights.shape[0],1))
         layer0_inputs_topics.append(T.dot(weights, Topic))
@@ -145,6 +145,7 @@ def train_conv_net(datasets,datasets_weights,
     if non_static:
         #if word vectors are allowed to change, add them as model parameters
         params += [Words]   #params are model parameters
+        params += [Topics]
     cost = classifier.negative_log_likelihood(y) 
     dropout_cost = classifier.dropout_negative_log_likelihood(y)           
     grad_updates = sgd_updates_adadelta(params, dropout_cost, lr_decay, 1e-6, sqr_norm_lim)
@@ -154,58 +155,57 @@ def train_conv_net(datasets,datasets_weights,
     np.random.seed(3435)
     if datasets[0].shape[0] % batch_size > 0:
         extra_data_num = batch_size - datasets[0].shape[0] % batch_size
-        train_set = np.random.permutation(datasets[0])   
+        random_index = np.random.permutation(np.range(datasets[0].shape[0])) 
+        train_set = datasets[0][random_index,:]
+        train_set_weights = datasets_weights[0][random_index,:,:]
         extra_data = train_set[:extra_data_num]
+        extra_data_weights = train_set_weights[:extra_data_num]
         new_data=np.append(datasets[0],extra_data,axis=0)
+        new_data_weights = np.append(datasets_weights[0],extra_data_weights,axis = 0)
     else:
         new_data = datasets[0]
-    new_data = np.random.permutation(new_data)
+        new_data_weights = datasets_weights[0]
+    random_index = np.random.permutation(np.range(new_data.shape[0])) 
+    new_data = new_data[random_index]
+    new_data_weights = new_data_weights[random_index]
     n_batches = new_data.shape[0]/batch_size
     n_train_batches = int(np.round(n_batches*0.9))
-    if len(datasets)==3:
-        use_valid_set=True
-        train_set = new_data
-        val_set = datasets[1]
-        train_set_x, train_set_y = shared_dataset((train_set[:,:img_h],train_set[:,-1]))
-        val_set_x, val_set_y = shared_dataset((val_set[:,:img_h],val_set[:,-1]))
-        test_set_x = np.asarray(datasets[2][:,:img_h] ,"float32")
-        test_set_y = np.asarray(datasets[2][:,-1],"int32")
-        n_val_batches = int(val_set.shape[0] / batch_size)
-        val_model = theano.function([index], classifier.errors(y),
-            givens={
-                  x: val_set_x[index * batch_size: (index + 1) * batch_size],
-                  y: val_set_y[index * batch_size: (index + 1) * batch_size]})
   
-
+    test_set_x = np.asarray(datasets[1][:,:img_h] ,"float32")
+    test_set_x_topic = np.asarray(datasets_weights[1][:,:img_h,:] ,"float32")
+    test_set_y = np.asarray(datasets[1][:,-1],"int32")
+    if use_valid_set:
+        train_set = new_data[:n_train_batches*batch_size,:]
+        train_set_weights = new_data_weights[:n_train_batches*batch_size,:,:]
+        val_set = new_data[n_train_batches*batch_size:,:]
+        val_set_weights = new_data_weights[n_train_batches*batch_size:,:,:]     
+        train_set_x, train_set_x_topic, train_set_y = shared_dataset((train_set[:,:img_h],train_set_weights[:,:img_h,:],train_set[:,-1]))
+        val_set_x, val_set_x_topic, val_set_y = shared_dataset((val_set[:,:img_h],val_set_weights[:,:img_h,:],val_set[:,-1]))
+        n_val_batches = n_batches - n_train_batches
+        val_model = theano.function([index], classifier.errors(y),
+                givens={
+                x: val_set_x[index * batch_size: (index + 1) * batch_size],
+                x_topic: val_set_x_topic[index * batch_size: (index + 1) * batch_size],
+                y: val_set_y[index * batch_size: (index + 1) * batch_size]})
     else:
-        test_set_x = np.asarray(datasets[1][:,:img_h] ,"float32")
-        test_set_y = np.asarray(datasets[1][:,-1],"int32")
-        if use_valid_set:
-            train_set = new_data[:n_train_batches*batch_size,:]
-            val_set = new_data[n_train_batches*batch_size:,:]     
-            train_set_x, train_set_y = shared_dataset((train_set[:,:img_h],train_set[:,-1]))
-            val_set_x, val_set_y = shared_dataset((val_set[:,:img_h],val_set[:,-1]))
-            n_val_batches = n_batches - n_train_batches
-            val_model = theano.function([index], classifier.errors(y),
-                 givens={
-                    x: val_set_x[index * batch_size: (index + 1) * batch_size],
-                    y: val_set_y[index * batch_size: (index + 1) * batch_size]})
-        else:
-            train_set = new_data[:,:]    
-            train_set_x, train_set_y = shared_dataset((train_set[:,:img_h],train_set[:,-1]))  
+        train_set = new_data[:,:]    
+        train_set_x, train_set_x_topic, train_set_y = shared_dataset((train_set[:,:img_h],train_set_weights[:,:img_h,:],train_set[:,-1]))  
             
     #make theano functions to get train/val/test errors
     test_model = theano.function([index], classifier.errors(y),
              givens={
                 x: train_set_x[index * batch_size: (index + 1) * batch_size],
+                x_topic: val_set_x_topic[index * batch_size: (index + 1) * batch_size],
                 y: train_set_y[index * batch_size: (index + 1) * batch_size]})               
     train_model = theano.function([index], cost, updates=grad_updates,
           givens={
             x: train_set_x[index*batch_size:(index+1)*batch_size],
+            x_topic: val_set_x_topic[index * batch_size: (index + 1) * batch_size],
             y: train_set_y[index*batch_size:(index+1)*batch_size]})     
     test_pred_layers = []
     test_size = test_set_x.shape[0]
     test_layer0_input = Words[T.cast(x.flatten(),dtype="int32")].reshape((test_size,1,img_h,Words.shape[1]))
+    
     for conv_layer in conv_layers:
         test_layer0_output = conv_layer.predict(test_layer0_input, test_size)
         test_pred_layers.append(test_layer0_output.flatten(2))
